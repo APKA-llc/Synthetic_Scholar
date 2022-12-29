2dfc2e7153df039a60ca1f6ffb52effcc139dca9
Alex Kranias <alexander.kranias@gmail.com>
2022-12-29 11:56:11 -0500

    11:56AM

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 import re
 import time
 
-API_KEY = "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"
+API_KEY = "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"
 model_engine = "text-davinci-002"
 
 
@@ -20,7 +20,7 @@ def main():
     # Get the generated text
     # generated_text = response["choices"][0]["text"]
 
-    subjects = open('topics.txt', 'r')
+    subjects = open('topics2.txt', 'r')
 
     current_subject = ""
     list_of_topics = []
@@ -43,13 +43,13 @@ def main():
 
         for index, topic in enumerate(list_of_topics):
             current_topic = list_of_topics[index]
-            #prompt = "Write an extremely detailed study guide on " + current_topic + " for " + current_subject + ". Talk about all aspects of the topic. Include relevant definitions and equations! Also include practice problems when possible."
-            prompt = "Write a multiple choice test on " + current_topic + " for " + current_subject + " with an answer key at the end."
+            prompt = "Write extremely detailed lecture notes on " + current_topic + " for the course " + current_subject + ". Talk about all aspects of the topic. Include relevant definitions and equations! Also include practice problems when possible."
+            #prompt = "Write a multiple choice test on " + current_topic + " for " + current_subject + " with an answer key at the end."
 
             response = openai.Completion.create(
                 engine=model_engine,
                 prompt=prompt,
-                temperature=0.0,
+                temperature=0.75,
                 max_tokens=3800,
                 top_p=1,
                 frequency_penalty=0,
